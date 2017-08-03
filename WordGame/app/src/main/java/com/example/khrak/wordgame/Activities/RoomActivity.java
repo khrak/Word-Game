8c75fe126d1fdef626e4a81523cde5f6d200f4db
khrak <msakh12@freeuni.edu.ge>
2017-08-03 21:57:36 +0400

    Add drawing room

--- WordGame/app/src/main/java/com/example/khrak/wordgame/Activities/RoomActivity.java
+++ WordGame/app/src/main/java/com/example/khrak/wordgame/Activities/RoomActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -14,6 +17,7 @@ import com.android.volley.toolbox.Volley;
 import com.example.khrak.wordgame.R;
 import com.example.khrak.wordgame.communication.CommunicationManager;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -37,9 +41,160 @@ public class RoomActivity extends AppCompatActivity {
                 leaveRoom();
             }
         });
+
+        drawRoom(roomId, username);
     }
 
-    public void leaveRoom(){
+    private void drawRoom(int roomId, String username) {
+
+        String url ="http://amimelia-001-site1.itempurl.com/api/gamelobby/GetRoom?userName=" +
+                username + "&roomId=" + roomId;
+
+        final com.android.volley.RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(final String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            String roomName = "";
+                            String ownerName = "";
+
+                            if (jsonObject.has("OwnerUserName")) {
+                                ownerName = jsonObject.getString("OwnerUserName");
+
+                                TextView ownerView = (TextView) findViewById(R.id.owner_image_view);
+
+                                ownerView.setText(ownerName);
+                            }
+
+                            if (jsonObject.has("RoomName")) {
+                                roomName = jsonObject.getString("RoomName");
+
+                                TextView roomNameView = (TextView) findViewById(R.id.room_title);
+
+                                roomNameView.setText(roomName);
+                            }
+
+                            ImageView imageView1 = (ImageView) findViewById(R.id.profile_image1);
+                            ImageView imageView2 = (ImageView) findViewById(R.id.profile_image2);
+                            ImageView imageView3 = (ImageView) findViewById(R.id.profile_image3);
+                            ImageView imageView4 = (ImageView) findViewById(R.id.profile_image4);
+                            ImageView imageView5 = (ImageView) findViewById(R.id.profile_image5);
+
+                            TextView nameView1 = (TextView) findViewById(R.id.profile_name1);
+                            TextView nameView2 = (TextView) findViewById(R.id.profile_name2);
+                            TextView nameView3 = (TextView) findViewById(R.id.profile_name3);
+                            TextView nameView4 = (TextView) findViewById(R.id.profile_name4);
+                            TextView nameView5 = (TextView) findViewById(R.id.profile_name5);
+
+                            if (jsonObject.has("MemberUsers")) {
+                                JSONArray array = jsonObject.getJSONArray("MemberUsers");
+
+                                updateViews(ownerName, imageView1, nameView1, array.getJSONObject(0));
+
+                                if (array.length() > 1) {
+                                    updateViews(ownerName, imageView1, nameView1, array.getJSONObject(1));
+                                }
+
+                                if (array.length() > 2) {
+                                    updateViews(ownerName, imageView1, nameView1, array.getJSONObject(2));
+                                }
+
+                                if (array.length() > 3) {
+                                    updateViews(ownerName, imageView1, nameView1, array.getJSONObject(3));
+                                }
+
+                                if (array.length() > 4) {
+                                    updateViews(ownerName, imageView1, nameView1, array.getJSONObject(4));
+                                }
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!" + error);
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    private void updateViews(String ownerName, ImageView imageView1, TextView nameView1, JSONObject jsonObject) {
+
+        try {
+            String username = jsonObject.getString("UserName");
+            int iconid = jsonObject.getInt("IconId");
+
+            nameView1.setText(username);
+
+            int sourceid = 0;
+
+            if (iconid == 1) {
+                sourceid = R.drawable.avatar_icon1;
+            }
+
+            if (iconid == 2) {
+                sourceid = R.drawable.avatar_icon1;
+            }
+
+            if (iconid == 3) {
+                sourceid = R.drawable.avatar_icon2;
+            }
+
+            if (iconid == 4) {
+                sourceid = R.drawable.avatar_icon3;
+            }
+
+            if (iconid == 5) {
+                sourceid = R.drawable.avatar_icon4;
+            }
+
+            if (iconid == 5) {
+                sourceid = R.drawable.avatar_icon5;
+            }
+
+            if (iconid == 6) {
+                sourceid = R.drawable.avatar_icon6;
+            }
+
+            if (iconid == 7) {
+                sourceid = R.drawable.avatar_icon7;
+            }
+
+            if (iconid == 8) {
+                sourceid = R.drawable.avatar_icon8;
+            }
+
+            if (iconid == 9) {
+                sourceid = R.drawable.avatar_icon9;
+            }
+
+            imageView1.setImageResource(sourceid);
+
+            if (ownerName.equals(username)) {
+                ImageView ownerImage = (ImageView) findViewById(R.id.owner_image);
+
+                ownerImage.setImageResource(sourceid);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void leaveRoom() {
 
         String url ="http://amimelia-001-site1.itempurl.com/api/Gamelobby/LeaveRoom?userName="
                 + CommunicationManager.getInstance().getUserName();

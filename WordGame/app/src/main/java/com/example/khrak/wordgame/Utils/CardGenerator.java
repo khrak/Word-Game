af5ca6e1a7e35d8bfc9339d196383be0423e356f
amimelia <meliaamirani@gmail.com>
2017-08-05 18:54:39 +0400

    added cards generator in utils

---
+++ WordGame/app/src/main/java/com/example/khrak/wordgame/Utils/CardGenerator.java
@@ -0,0 +1,14 @@
+package com.example.khrak.wordgame.Utils;
+
+import com.example.khrak.wordgame.Game.Card;
+
+/**
+ * Created by melia on 8/5/2017.
+ */
+
+public class CardGenerator {
+
+    public static Card[] getRandomCards(int cardsNum){
+        return new Card[2];
+    }
+}

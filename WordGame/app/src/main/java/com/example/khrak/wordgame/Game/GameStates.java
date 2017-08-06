f6b60cb74b6dd35c26a4d1a674564e282ef487f9
amimelia <meliaamirani@gmail.com>
2017-08-06 09:17:52 +0400

    updated game states.

--- WordGame/app/src/main/java/com/example/khrak/wordgame/Game/GameStates.java
+++ WordGame/app/src/main/java/com/example/khrak/wordgame/Game/GameStates.java
@@ -9,5 +9,6 @@ public class GameStates {
     public static final int GAME_BETTING_STARTED = 2;
     public static final int GAME_BETTING_FINISHED = 3;
     public static final int GAME_OVER = 5;
+    public static final int NEW_ROUND = 6;
     public static final int ROUND_FINISHED = 4;
 }
